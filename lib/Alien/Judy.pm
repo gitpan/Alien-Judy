54a03272be3f48de1a0cb5c9a8d008ab73539cbc
Joshua ben Jore <jjore@cpan.org>
2008-12-20 09:20:10 +0000

    Import of JJORE/Alien-Judy-0.01 from CPAN.
    gitpan-cpan-distribution: Alien-Judy
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JJORE/Alien-Judy-0.01.tar.gz
    gitpan-cpan-author:       JJORE
    gitpan-cpan-maturity:     released

---
+++ lib/Alien/Judy.pm
@@ -0,0 +1,75 @@
+package Alien::Judy;
+
+use version; our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Alien::Judy - A wrapper for installing the libJudy C library
+
+=head1 DESCRIPTION
+
+Alien::Judy is a wrapper to ease installing the libJudy C library from
+L<http://judy.sourceforge.net>. L<Judy::HS> depends on this to ensure
+that libJudy is available for installation.
+
+=head1 AUTHOR
+
+Joshua ben Jore, C<< <jjore at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-alien-judy at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Alien-Judy>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Alien::Judy
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Alien-Judy>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Alien-Judy>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Alien-Judy>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Alien-Judy/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Joshua ben Jore, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Alien::Judy
