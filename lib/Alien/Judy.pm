069716ce57a6c3607773f346d6ccaf1759c1b2b7
Joshua ben Jore <jjore@cpan.org>
2010-06-06 18:04:19 +0000

    Import of JJORE/Alien-Judy-0.12 from CPAN.
    gitpan-cpan-distribution: Alien-Judy
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         JJORE/Alien-Judy-0.12.tar.gz
    gitpan-cpan-author:       JJORE
    gitpan-cpan-maturity:     released

--- lib/Alien/Judy.pm
+++ lib/Alien/Judy.pm
@@ -20,9 +20,9 @@ sub inc_dirs {
         grep { defined() && length() && -d }
         unique(
             Cwd::getcwd(),
-            map { File::Spec->catdir( $_, 'include' ) }
+            map { File::Spec->catdir( $_, 'Alien', 'Judy' ) }
             grep { defined() && length() }
-            @Config::Config{qw( siteprefixexp prefixexp )}
+            @Config::Config{qw(sitearchexp sitearch)}
         );
 }
 
@@ -36,6 +36,6 @@ sub lib_dirs {
         );
 }
 
-$VERSION = '0.11';
+$VERSION = '0.12';
 
 1;
